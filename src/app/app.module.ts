3f3ec5950cc29450e2a153ff882ce10e7435db9d
emmanuelmacharia <samuelmarsha@outlook.com>
2020-02-25 22:08:20 +0300

    the sidenav menu; only the routing and the icons remain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavComponent
+    TopNavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
