c23fe790fbd1f647ce2086613d1af3dcfbf2696e
emmanuelmacharia <samuelmarsha@outlook.com>
2020-02-26 14:09:07 +0300

    completed the routing on the two views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DataCardComponent } from './data-card/data-card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: DataCardComponent },
+  { path: 'bulkpayments', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [DashboardComponent, DataCardComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
-import { DataCardComponent } from './data-card/data-card.component';
+// import { DataCardComponent } from './data-card/data-card.component';
 import { DataCardNavComponent } from './data-card-nav/data-card-nav.component';
 import { DatatableComponent } from './datatable/datatable.component';
 import { PopulatedExpComponent } from './populated-exp/populated-exp.component';
 import { DashboardDatacardNavComponent } from './dashboard-datacard-nav/dashboard-datacard-nav.component';
 import { DashboardNavDatacard2Component } from './dashboard-nav-datacard2/dashboard-nav-datacard2.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    routingComponents,
     TopNavComponent,
     SideNavComponent,
-    DataCardComponent,
     DataCardNavComponent,
     DatatableComponent,
     PopulatedExpComponent,
     DashboardDatacardNavComponent,
-    DashboardNavDatacard2Component
+    DashboardNavDatacard2Component,
   ],
   imports: [
     BrowserModule,
