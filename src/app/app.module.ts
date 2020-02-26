dafae0e43de35936e74c59389c56887e00b7eaa9
emmanuelmacharia <samuelmarsha@outlook.com>
2020-02-26 08:30:17 +0300

    added the sidenav and created the datacard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { DataCardComponent } from './data-card/data-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavComponent,
-    SideNavComponent
+    SideNavComponent,
+    DataCardComponent
   ],
   imports: [
     BrowserModule,
